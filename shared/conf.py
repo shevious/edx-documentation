c33aa1a8422ecd6fa82545de3e8cf1816f8294cf
shevious <shevious@yahoo.com>
2017-06-01 10:08:33 +0900

    fixes conf.py - remove pngmath

--- shared/conf.py
+++ shared/conf.py
@@ -29,7 +29,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
     'sphinx.ext.coverage',
-    'sphinx.ext.pngmath',
+#    'sphinx.ext.pngmath',
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.extlinks',
